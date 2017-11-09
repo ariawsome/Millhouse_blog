cfaadbd0db3c9f7e0032644c8484f8b3874ce71d
Filip Ingemarsson <Filip@Filips-MacBook-Pro.local>
2017-11-09 17:17:51 +0100

    registering form added br

--- register_user.php
+++ register_user.php
@@ -30,20 +30,19 @@
       
            <label for="forname"> Forname: </label>
           <input type="text" name="forname" required>
-          
+          <br>
           <label for="lastname"> Lastname </label>
           <input type="text" name="lastname" required>
-          
+          <br>
           <label for="email"> Email: </label>
           <input type="email" name="email" required>
-          
+          <br>
           <label for="username"> Username </label>
           <input type="text" name="username" required>
-        
+          <br>
           <label for="password"> Password </label>
           <input type="password" name="password" required>
-
-
+          <br>
           <input type="submit" class="btn btn-primary">
           
       </form>
