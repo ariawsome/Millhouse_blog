96926953bbc9c605207d5fc20c0b9aaa658d36be
Hanna Landerus <hannalanderus@Hannas-Air-2.lan>
2017-11-23 17:33:09 +0100

    change details in  comment and post

--- partials/head.php
+++ partials/head.php
@@ -17,7 +17,7 @@
        
         <a href="javascript:void(0)" class="w3-left w3-button w3-transparent w3-xxxlarge" onclick="w3_open()">☰</a>
         
-        <div class="logo"><img src="image/jumbotron/millhouse-logo-white.svg"></div>
+        <div class="logo"><a href="index.php"><img src="image/jumbotron/millhouse-logo-white.svg" alt="logo"></a></div>
         
     </header>
     
