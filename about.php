55111c9259d7eb94a49a4bb45bc0e828b39bfe4b
Helena Edblad <Helena@Helenas-MacBook-Pro.local>
2017-11-24 12:05:42 +0100

    added about

---
+++ about.php
@@ -0,0 +1,17 @@
+<?php
+    session_start();
+    require 'partials/head.php';
+?>
+    
+    <main class="main">
+        <div class="about">
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque, sequi neque ipsam. Obcaecati, magnam delectus, debitis itaque impedit recusandae? Hic quos ipsum quae quisquam temporibus facere deserunt sint harum placeat.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque, sequi neque ipsam. Obcaecati, magnam delectus, debitis itaque impedit recusandae? Hic quos ipsum quae quisquam temporibus facere deserunt sint harum placeat.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque, sequi neque ipsam. Obcaecati, magnam delectus, debitis itaque impedit recusandae? Hic quos ipsum quae quisquam temporibus facere deserunt sint harum placeat.</p>
+        </div>        
+    </main>
+    
+<?php
+    require 'partials/footer.php'; 
+?>
