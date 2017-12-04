8cf42754bb2a9ae978694217e5348a641cce6828
Hanna Landerus <hannalanderus@Hannas-Air-2.lan>
2017-12-04 13:42:10 +0100

    style footer

--- partials/footer.php
+++ partials/footer.php
@@ -1,11 +1,23 @@
-
 <footer>
     <div class="footer-content">
-       <p>Villa Villekulla 20,Stockholm län</p>
-       <p>happyunicorn@ninjaweb.se</p>
-       <p>Ring oss: (+43)075321892</p>
-   
- <!--     <li><a href=“http://www.facebook.com“>
+        <i class="fa fa-facebook-square" aria-hidden="true"></i>
+        <i class="fa fa-instagram" aria-hidden="true"></i>
+        <i class="fa fa-twitter-square" aria-hidden="true"></i>
+
+        <p>Villa Villekulla 20,Stockholm län</p>
+        <p>happyunicorn@ninjaweb.se</p>
+        <p>Ring oss: (+43)075321892</p>
+
+
+    </div>
+</footer>
+
+</body>
+
+</html>
+
+
+<!--<li><a href=“http://www.facebook.com“>
            <img src=“image/footer_logos/facebook.svg”>
        </a></li>
        <li><a href=“http://www.twitter.com”>
@@ -13,9 +25,4 @@
        </a></li>
        <li><a href=“http://www.instagram.com”>
            <img src=“image/footer_logos/instagram.svg”/>
-       </a></li> -->
-    </div>
-</footer>
-   
-</body>
-</html>
+       </a></li>-->
