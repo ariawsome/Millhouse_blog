7f13e4a5710b957c8e6c0d2b18f17c0aaf07e36e
Flippen97 <32750647+Flippen97@users.noreply.github.com>
2017-12-08 13:43:22 +0100

    Update database.php

--- partials/database.php
+++ partials/database.php
@@ -1,7 +1,13 @@
 <?php
 
 $pdo = new PDO(
-  "mysql:host=localhost;dbname=filiping_Millhouse-blog;charset=utf8",
-  "filiping_Filip",
-  "jT7sVuh&DKkx"
-);
+ "mysql:host=localhost;dbname=our_database;charset=utf8",
+ "root",
+ "root"
+  );
+
+//$pdo = new PDO(
+// "mysql:host=localhost;dbname=filiping_Millhouse-blog;charset=utf8",
+//  "filiping_Filip",
+//  "jT7sVuh&DKkx"
+//);
