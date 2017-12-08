3766ac35f351edd12e3193b899f168a0e29841b9
Filip <filip.ingemarsson@live.com>
2017-12-08 13:40:24 +0100

    Final touches

--- partials/database.php
+++ partials/database.php
@@ -1,7 +1,7 @@
 <?php
 
 $pdo = new PDO(
-  "mysql:host=localhost;dbname=our_database;charset=utf8",
-  "root",
-  "root"
+  "mysql:host=localhost;dbname=filiping_Millhouse-blog;charset=utf8",
+  "filiping_Filip",
+  "jT7sVuh&DKkx"
 );
